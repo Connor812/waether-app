a7c38a20fe4ad546ddd388449ace573281c20858
Connor Savoy <connorsavoy812@gmail.com>
2023-01-28 16:19:23 -0500

    Completed the fetch function for the url's to make sure i can grab the information from the weather api site correctly

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,12 +1,67 @@
-let weatherUrl = 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=2ca1c9e6889192710f3b59dc0b31f2bf';
+place = prompt("Enter location: ");;
+let weatherUrl;
+let locationUrl;
+let lan;
+let lon;
+let day;
+getLocationUrl();
+console.log(place);
 
 
 
-fetch(weatherUrl)
-.then(function (response) {
-return response.json();
-})
-.then(function (data) {
 
-    console.log(data);
-});
+function getLocationUrl() {
+
+    locationUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + place + '&limit=10&appid=2ca1c9e6889192710f3b59dc0b31f2bf';
+    console.log(locationUrl);
+
+    fetch(locationUrl)
+        .then(function (locationResponse) {
+            return locationResponse.json();
+        })
+        .then(function (locationData) {
+            console.log(locationData);
+
+            lat = locationData[0].lat;
+            lon = locationData[0].lon;
+            console.log(lat);
+            console.log(lon);
+            getWeatherUrl();
+        })
+}
+
+
+function getWeatherUrl() {
+
+    console.log(lat);
+    console.log(lon);
+
+   
+    weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=2ca1c9e6889192710f3b59dc0b31f2bf';
+    console.log(weatherUrl);
+
+
+
+    fetch(weatherUrl)
+        .then(function (weatherResponse) {
+            return weatherResponse.json();
+        })
+        .then(function (weatherData) {
+
+            console.log(weatherData);
+            
+            day = weatherData.list;
+            console.log(day);
+            renderForcast()
+
+
+        });
+}
+
+function renderForcast() {
+    for (let i = 0; i < day.length;) {
+        console.log(i);
+        console.log(day[i]);
+        i = i + 8;
+    }
+}
